9767a42ea2cb089ab26ff4b34557cb3a4c3c7f19
Bearchat <283938162@qq.com>
2019-12-06 17:36:32 +0800

    新增部分页面

--- src/components/library/LibraryIndex.vue
+++ src/components/library/LibraryIndex.vue
@@ -25,20 +25,20 @@
   import Books from './Books'
 
   export default {
-    name: 'AppLibrary',
-    components: {Books, SideMenu},
-    methods: {
-      listByCategory () {
-        var _this = this
-        var cid = this.$refs.sideMenu.cid
-        var url = 'categories/' + cid + '/books'
-        this.$axios.get(url).then(resp => {
-          if (resp && resp.status === 200) {
-            _this.$refs.booksArea.books = resp.data
-          }
-        })
+      name: 'AppLibrary',
+      components: {Books, SideMenu},
+      methods: {
+        listByCategory () {
+          var _this = this
+          var cid = this.$refs.sideMenu.cid
+          var url = 'categories/' + cid + '/books'
+          this.$axios.get(url).then(resp => {
+            if (resp && resp.status === 200) {
+              _this.$refs.booksArea.books = resp.data
+            }
+          })
+        }
       }
-    }
   }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import AppIndex from '@/components/home/AppIndex'
 import Login from '@/components/Login'
+import Register from '@/components/Register'
 import Test from '@/components/Test'
 import Home from '@/components/Home'
 import LibraryIndex from '../components/library/LibraryIndex'
@@ -29,6 +30,11 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
     },
     {
       path:'/home',
